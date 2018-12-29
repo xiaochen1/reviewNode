c0669ced79b354fef5b262c9bdf0915888ca06ef
chenxiaoqiang <chenxiaoqiang@mixlinker.com>
2018-12-29 18:00:19 +0800

    用户注册

--- 6.db.js
+++ 6.db.js
@@ -1,33 +1,69 @@
 const express = require("express");
 
-
 const DB = require("./model/db.js");
+const formidable = require("formidable");
 
 let app = express();
+app.set("view engine", "ejs");
+
+
+app.use(express.static("./static"));
+app.use(express.static("./upload"));
+
 
 app.get("/", (req, res) => {
     res.send("跟路由");
 });
-
 app.get("/dbjson", (req, res) => {
-    // DB.connect("reviewNode", (err, db) => {
-    //     if (err) {
-    //         console.log("连接数据库失败");
-    //         console.log(err);
-    //         return;
-    //     }
+    let page_index = parseInt(req.query.page_index);
+    let page_size = parseInt(req.query.page_size);
+    let condition = {};
+    condition.score = { $lt: parseInt(req.query.score) };
+    console.log(page_index, page_size);
+    console.log(condition);
 
-    //     console.log(db.collection("user").find());
-    // });
-
-    DB.getDocumentsByColName("reviewNode", "user", { score: { $lt: 60 } }, (err, data) => {
+    DB.getDocumentsByColName("reviewNode", "user", condition, { page_index: page_index, page_size: page_size }, (err, data) => {
         if (err) {
             console.log("连接失败");
             res.send("连接失败啦");
         }
-        res.send(JSON.stringify(data));
+        console.log(JSON.stringify(data, null, 4));
+        res.send(JSON.stringify(data, null, 4));
     });
 });
+
+app.get("/register", (req, res) => {
+    res.render("register");
+});
+
+
+app.post("/doregister", (req, res) => {
+
+    let form = new formidable.IncomingForm();
+    form.uploadDir = "/upload";
+
+    form.parse(req, (err, fields, files) => {
+        if (err) {
+            res.send("注册失败");
+            return;
+        }
+        let regisInfo = {};
+        regisInfo.username = fields.username;
+        regisInfo.password = fields.password;
+        regisInfo.age = fields.age;
+        DB.addUser(regisInfo, (err, result) => {
+            if (err) {
+                res.send("添加用户失败");
+                return;
+            }
+            res.send(JSON.stringify(result));
+        });
+    });
+
+});
+
+
+
 
 app.listen(8888);
 console.log("listening on 8888");
--- model/db.js
+++ model/db.js
@@ -3,10 +3,17 @@ const assert = require("assert");
 const dburl = "mongodb://localhost:27017";
 const dbName = "reviewNode";
 
+
 exports._connect = _connect;
 exports.getDocumentsByColName = getDocumentsByColName;
+exports.addUser = addUser; // 添加用户
 
 //数据库连接
+/**
+ * 
+ * @param {*数据库名称*} dbname 
+ * @param {*回调函数*} cb 
+ */
 function _connect(dbname, cb) {
 
     let client = new MongoClient(dburl, { useNewUrlParser: true });
@@ -41,34 +48,109 @@ function _connect(dbname, cb) {
 
 
 //根据集合名称获取集合数据
-function getDocumentsByColName(dbname, colname, option = {}, callback) {
+/**
+ * 
+ * @param {数据库名称} dbname 
+ * @param {集合名称} colname 
+ * @param {查询时条件} option 
+ * @param {回调函数} callback 
+ */
+function getDocumentsByColName(dbname, colname, option = {}, opquery = { page_size: 10, page_index: 1 }, callback) {
     _connect(dbname, (err, db, client) => {
-        // if(!assert.equal(null, err)) {
-        //     callback(err, null);
-        //     client.close();
-        //     console.log("失败");
-        //     return;
-        // }
+
         assert.equal(err, null);
         console.log("getDocumentsByColName-------");
         console.log(db);
         console.log(client);
         assert.equal(null, err);
-        let res = [];
         console.log("根据集合名称获取集合中的数据");
         console.log(dbname, colname);
         const collection = db.collection(colname);
         console.log(collection);
-        // collection.find(option).toArray((err, docs) => {
+
+        let result = {
+            code: 500,
+            msg: "",
+            page_index: opquery.page_index || 1,
+            page_size: opquery.page_size || 10,
+            total_records: 0,
+            total_pages: 0,
+            data: []
+        };
+
+        let cursor = collection.find(option);
+
+        cursor.limit(result.page_size).skip((opquery.page_index - 1) * opquery.page_size).toArray((err, docs) => {
+            assert.equal(err, null);
+            cursor.count(false, {}, (err, count) => {
+                assert.equal(err, null);
+                result.code = 200;
+                result.msg = "查询成功";
+                result.total_records = count;
+                result.total_pages = Math.ceil(count / result.page_size);
+                result.data = docs;
+
+                callback(null, result);
+                client.close();
+
+            });
+        });
+
+        // cursor.count(true, { limit: opquery.page_size, skip: (opquery.page_index - 1) * opquery.page_size }, (err, count) => {
+        //     assert.equal(err, null);
+
+        //     cursor.toArray((err, docs) => {
+        //         assert.equal(err, null);
+        //         result.data = docs;
+        //         result.page_index = opquery.page_index || result.page_index;
+        //         result.page_size = opquery.page_size || result.page_size;
+        //         result.total_pages = Math.ceil(docs.length / result.page_size);
+        //         result.total_records = count;
+
+        //         callback(null, result);
+        //         client.close();
+        //     });
+        // })
+
+        // collection.find({}).limit(3).skip(2).toArray((err, docs) => {
         //     assert.equal(null, err);
+        //     let result = {};
         //     callback(null, docs);
         //     client.close();
         // });
+    });
+}
 
-        collection.find({}).limit(3).skip(2).toArray((err, docs) => {
-            assert.equal(null, err);
-            callback(null, docs);
+//------ 用户（user）  相关操作-----------------------------------------------------
+
+// 增
+function addUser(info, cb) {
+    _connect(dbName, (err, db, client) => {
+        assert.equal(err, null);
+        let user_coll = db.collection("user");
+        user_coll.insertOne(info, {}, (err, result) => {
+            console.log(assert.equal(err, null));
+            console.log(result);
+            cb(null, result);
             client.close();
         });
     });
+}
+
+
+
+
+// 查
+function getUserInfo(info, cb) {
+
+}
+
+
+
+/**
+ * 
+ * @param  info  用户信息
+ */
+function register(info) {
+    _connect();
 }
